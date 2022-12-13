82c0188d4ec9776489282da4cda1105eb9c5bda6
CadetAkins <89479675+CadetAkins@users.noreply.github.com>
2022-12-13 09:46:38 -0600

    Update rooms.py

--- monster/core/rooms.py
+++ monster/core/rooms.py
@@ -4,8 +4,11 @@ class Room(object):
     def __init__(self, channel):
         self.members = []
         self.channel = channel
+        self.author = None
 
     def add_member(self, member):
+        if len(self.members) == 0:
+            self.author = member
         self.members.append(member)
 
     def remove_member(self, member):
