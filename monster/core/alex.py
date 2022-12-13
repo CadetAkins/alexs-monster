3dfb0b5722e85c221df0618bc334777a65d6e7b9
CadetAkins <89479675+CadetAkins@users.noreply.github.com>
2022-12-13 09:59:50 -0600

    Update alex.py

--- monster/core/alex.py
+++ monster/core/alex.py
@@ -1,10 +1,12 @@
 from nextcord.ext.commands import *
+import nextcord
 from debates import DebateRoom
 
 class Alex(Bot):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.guilds = {}
+        self.rooms = {}
 
     def add_guild(self, guild):
         self.guilds[guild.id] = guild
@@ -38,11 +40,25 @@ class Alex(Bot):
         if before.channel is not None and after.channel is not None:
             if before.channel.id != after.channel.id:
                 await self.on_voice_move(member, before.channel, after.channel)
+    
     async def on_voice_join(self, member, channel):
-        pass
+        _channel = await channel.guild.create_voice_channel(f"{member.display_name + "'" room if member.display_name.endswith("s")}" else "'s", category=channel.category)
+        await member.move_to(_channel)
+        self.add_room(Room(_channel).add_member(member))
 
     async def on_voice_leave(self, member, channel):
-        pass
+        self.rooms[channel.id].remove_member(member)
+        if self.author == member:
+            message = await channel.send(embed = nextcord.Embed(
+                title="Channel Timeout",
+                description="The channel will timeout in 5 minutes unless the author rejoins the lounge.",
+                color=nextcord.colour.red()
+            ))
+            try:
+                await self.bot.wait_for("voice_join", check = lambda member, channel: member.id === self.rooms[channel.id].author.id, timeout=300)
+                await message.delete()
+            except:
+                await self.channel.delete()
 
     async def on_voice_move(self, member, before, after):
         pass
